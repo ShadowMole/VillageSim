1065dc140d74ce30689ae51ee2a6e4ae142658af
Steven Bruman <steven.bruman@yahoo.com>
2018-12-04 08:50:04 -0500

    Add files via upload

--- Farmer.java
+++ Farmer.java
@@ -19,6 +19,135 @@ public class Farmer extends Job {
     private boolean hasWool;
     private boolean hasMeat;
 
+    public Farmer(){
+        super();
+
+        needs = new ItemType[]{ItemType.SHOVEL, ItemType.PLOW, ItemType.BUCKET, ItemType.SHEARS, ItemType.CART};
+        produces = new ItemType[]{ItemType.CARROT, ItemType.POTATO, ItemType.WHEAT, ItemType.RAWMEAT, ItemType.WOOL, ItemType.MILK};
+    }
+
+    @Override
+    public void work(ArrayList<Item> inven, int intel){
+        Iterator<Item> i = inven.iterator();
+        while(i.hasNext()){
+            Item j = i.next();
+            if(j instanceof Animal){
+                ((Animal) j).live();
+                if(((Animal) j).getLife() == 0){
+                    boolean found = false;
+                    for(int k = 0; !found && k < inven.size(); k++){
+                        if(inven.get(k) instanceof Consumable && inven.get(k).getName().equals("Raw Meat")) {
+                            ((Consumable) inven.get(k)).addMore(((Animal) j).getFood());
+                            found = true;
+                        }
+                    }
+                    i.remove();;
+                }else if(Randomizer.getRandom(((Animal) j).getBirthRate()) == 0){
+                    for(int k = 0; k < ((Animal) j).getBirths(); k++){
+                        inven.add(ItemBuilder.newItem(j.getName(), 0));
+                    }
+                }
+            }
+        }
+        int cows = 0;
+        int sheep = 0;
+        for(int j = 0; j < inven.size(); j++){
+            if(inven.get(j) instanceof Animal){
+                if(inven.get(j).getName().equals("Cow")){
+                    cows++;
+                }else if(inven.get(j).getName().equals("Sheep)")){
+                    sheep++;
+                }
+            }
+        }
+        if(cows > 0){
+            hasCows = true;
+        }else{
+            hasCows = false;
+        }
+
+        if(sheep > 0){
+            hasSheep = true;
+        }else{
+            hasSheep = false;
+        }
+
+        for(int j = 0; j < intel; j++){
+            int work = Randomizer.getRandom(5);
+            switch (work){
+                case 0:
+                    milkCow(inven);
+                    break;
+                case 1:
+                    shearSheep(inven);
+                    break;
+                case 2:
+                    growCarrots(inven);
+                    break;
+                case 3:
+                    growPotatoes(inven);
+                    break;
+                case 4:
+                    growWheat(inven);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void initialInventory(ArrayList<Item> inven){
+
+    }
+
+    @Override
+    public void firstInventory(ArrayList<Item> inven){
+
+    }
+
+    public void milkCow(ArrayList<Item> inven){
+        if(hasCows && hasBucket && Randomizer.getRandom(7) == 0){
+            inven.add(ItemBuilder.newItem("Milk", 0));
+            boolean found = false;
+            Iterator<Item> it = inven.iterator();
+            while(!found && it.hasNext()){
+                Item i = it.next();
+                if(i instanceof Tool && i.getName().equals("Bucket")){
+                    ((Tool) i).use();
+                    found = true;
+                    if(((Tool) i).getDurability() == 0){
+                        it.remove();
+                        boolean flag = false;
+                        for(int j = 0; !flag && j < inven.size(); j++){
+                            if(inven.get(j) instanceof Tool && inven.get(j).getName().equals("Bucket")){
+                                hasBucket = true;
+                                flag = true;
+                            }
+                        }
+                        if(!flag){
+                            hasBucket = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void shearSheep(ArrayList<Item> inven){
+
+    }
+
+    public void growCarrots(ArrayList<Item> inven){
+
+    }
+
+    public void growPotatoes(ArrayList<Item> inven){
+
+    }
+
+    public void growWheat(ArrayList<Item> inven){
+
+    }
+
     public boolean isHasShovel() {
         return hasShovel;
     }
@@ -121,114 +250,5 @@ public class Farmer extends Job {
 
     public void setHasMeat(boolean hasMeat) {
         this.hasMeat = hasMeat;
-    }
-
-    public Farmer(){
-        super();
-
-        needs = new ItemType[]{ItemType.SHOVEL, ItemType.PLOW, ItemType.BUCKET, ItemType.SHEARS, ItemType.CART};
-        produces = new ItemType[]{ItemType.CARROT, ItemType.POTATO, ItemType.WHEAT, ItemType.RAWMEAT, ItemType.WOOL, ItemType.MILK};
-    }
-
-    @Override
-    public void work(ArrayList<Item> inven, int intel){
-        Iterator<Item> i = inven.iterator();
-        while(i.hasNext()){
-            Item j = i.next();
-            if(j instanceof Animal){
-                ((Animal) j).live();
-                if(((Animal) j).getLife() == 0){
-                    boolean found = false;
-                    for(int k = 0; !found && k < inven.size(); k++){
-                        if(inven.get(k) instanceof Consumable && inven.get(k).getName().equals("Raw Meat")) {
-                            ((Consumable) inven.get(k)).addMore(((Animal) j).getFood());
-                            found = true;
-                        }
-                    }
-                    i.remove();;
-                }else if(Randomizer.getRandom(((Animal) j).getBirthRate()) == 0){
-                    for(int k = 0; k < ((Animal) j).getBirths(); k++){
-                        inven.add(ItemBuilder.newItem(j.getName(), 0));
-                    }
-                }
-            }
-        }
-        int cows = 0;
-        int sheep = 0;
-        for(int j = 0; j < inven.size(); j++){
-            if(inven.get(j) instanceof Animal){
-                if(inven.get(j).getName().equals("Cow")){
-                    cows++;
-                }else if(inven.get(j).getName().equals("Sheep)")){
-                    sheep++;
-                }
-            }
-        }
-        if(cows > 0){
-            hasCows = true;
-        }else{
-            hasCows = false;
-        }
-
-        if(sheep > 0){
-            hasSheep = true;
-        }else{
-            hasSheep = false;
-        }
-
-        for(int j = 0; j < intel; j++){
-            int work = Randomizer.getRandom(5);
-            switch (work){
-                case 0:
-                    milkCow(inven);
-                    break;
-                case 1:
-                    shearSheep(inven);
-                    break;
-                case 2:
-                    growCarrots(inven);
-                    break;
-                case 3:
-                    growPotatoes(inven);
-                    break;
-                case 4:
-                    growWheat(inven);
-                    break;
-            }
-        }
-    }
-
-    @Override
-    public void initialInventory(ArrayList<Item> inven){
-
-    }
-
-    @Override
-    public void firstInventory(ArrayList<Item> inven){
-
-    }
-
-    public void milkCow(ArrayList<Item> inven){
-        if(hasCows && hasBucket && Randomizer.getRandom(7) == 0){
-            for(int i = 0; i < inven.size(); i++){
-                
-            }
-        }
-    }
-
-    public void shearSheep(ArrayList<Item> inven){
-
-    }
-
-    public void growCarrots(ArrayList<Item> inven){
-
-    }
-
-    public void growPotatoes(ArrayList<Item> inven){
-
-    }
-
-    public void growWheat(ArrayList<Item> inven){
-
     }
 }
